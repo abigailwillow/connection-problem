883769728967b4ca5dd4f8f1a9cc052febe5fcfc
Abigail de Joode <abbydiode@gmail.com>
2021-07-03 00:08:19 +0200

    Remove pawn actions and add more HUD elements

--- code/MinimalPlayer.cs
+++ code/MinimalPlayer.cs
@@ -17,26 +17,4 @@ partial class MinimalPlayer : Player {
 
 		base.Respawn();
 	}
-
-	public override void Simulate(Client client)
-	{
-		base.Simulate(client);
-
-		SimulateActiveChild(client, ActiveChild);
-
-		if (IsServer && Input.Pressed(InputButton.Attack1)) {
-			var ragdoll = new ModelEntity();
-			ragdoll.SetModel( "models/citizen/citizen.vmdl" );
-			ragdoll.Position = EyePos + EyeRot.Forward * 40;
-			ragdoll.Rotation = Rotation.LookAt(Vector3.Random.Normal);
-			ragdoll.SetupPhysicsFromModel(PhysicsMotionType.Dynamic, false);
-			ragdoll.PhysicsGroup.Velocity = EyeRot.Forward * 1000;
-		}
-	}
-
-	public override void OnKilled() {
-		base.OnKilled();
-
-		EnableDrawing = false;
-	}
 }
